a202e3d0a323e95e6740d0378d6e0bb8418e77b9
hmphne <3rdoperson@gmail.com>
2025-05-01 15:33:13 +0330

    feat: added RingBearer font

--- App.js
+++ App.js
@@ -1,8 +1,16 @@
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import AppNavigator from "./navigation/AppNavigator";
 import { AuthProvider } from "./utils/authProvider";
+import { useFonts } from "expo-font";
 
 export default function App() {
+const [fontsLoaded] = useFonts({
+  RingBearer: require("./assets/fonts/RingBearer.ttf"),
+});
+
+if (!fontsLoaded) return null;
+
+
   return (
     <AuthProvider>
       <GestureHandlerRootView style={{ flex: 1 }}>
